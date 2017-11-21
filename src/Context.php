1dbd5d9bf9e70343a30a1613c9cdeae3410719f2
Vincent Mi <miwenshu@gmail.com>
2017-11-21 22:33:39 +0800

    add unit test

--- src/Context.php
+++ src/Context.php
@@ -1,5 +1,6 @@
 <?php
-namespace  CCM;
+
+namespace CCM;
 /**
  * Calculation Context
  * result cache and provide a depends check
@@ -7,27 +8,71 @@ namespace  CCM;
  *
  * @author miwenshu@gmail.com
  */
+class CCMException extends \Exception
+{
+}
+
+/**
+ * 缺少公式运行的变量
+ * @package CCM
+ */
+class VariableMissingException extends CCMException
+{
+
+}
+
+/**
+ * Class InvalidMapException
+ * @package CCM
+ */
+class InvalidException extends CCMException
+{
+
+}
+
+class CyclicDependenceException extends CCMException
+{
+
+}
+
+class KeyOccupiedException extends CCMException
+{
+
+}
+
+
 class Context
 {
 
     public $prefix = 'cal.';
 
     public $prefixLength = 4;
-
+    public $rsetCount = 0;
     private $fields = [];
-
+    private $cache = [];
     private $map = [];
-
     private $callstack = [];
     private $callstackLevel = [];
-
     private $metas = [];
-
     private $interceptors = [];
-
     private $depends = [];
 
-    public $rsetCount = 0;
+    public function gets($keys, $level, $replaceDot = false)
+    {
+        if (!is_array($keys)) {
+            $keys = explode(',', $keys);
+        }
+        $data = [];
+        foreach ($keys as $key) {
+            if ($replaceDot) {
+                $k = str_replace('.', '_', $key);
+            } else {
+                $k = $key;
+            }
+            $data[$k] = $this->get($key, $level);
+        }
+        return $data;
+    }
 
     /**
      * 获取指定key的值，用于公式内部运算
@@ -37,101 +82,137 @@ class Context
      * @return mixed|null
      * @throws Exception
      */
-    public function get($key,$level)
+    public function get($key, $level)
     {
         $key = trim($key);
+
         //interceptor
-        $keys = explode('.',$key);
-        foreach($this->interceptors as $interceptor){
-            if($interceptor->match($this,$keys)){
-                $interceptor->perform($this,$keys,$key);
+        $keys = explode('.', $key);
+        foreach ($this->interceptors as $interceptor) {
+            if ($interceptor->match($this, $keys)) {
+                $interceptor->perform($this, $keys, $key);
             }
         }
+
+        if (isset($this->fields[$key])){
+            $callStackLen = count($this->callstack);
+            if($callStackLen > 0) {
+                $pKey = $this->callstack[$callStackLen-1];
+                $this->depends[$key][$pKey] = $pKey;
+            }
+            return $this->fields[$key];
+        }
+
+
         //callstack check
         if (in_array($key, $this->callstack)) {
             array_push($this->callstack, $key);
-            array_push($this->callstackLevel,$level);
+            array_push($this->callstackLevel, $level);
 
             $text = "Cyclic dependence error occur in resolve '$key'. please check call stack below：\n";
-            foreach ($this->callstack as $index => $item) {
-                $text .= "|-".str_repeat('--',$this->callstackLevel[$index]) . $item ."\n";
-            }
-            $text.="\n";
-            throw new \Exception($text);
+            $text .= $this->printCallstack(true);
+            $text .= "\n";
+            throw new CyclicDependenceException($text);
         }
         array_push($this->callstack, $key);
-        array_push($this->callstackLevel,$level);
+        array_push($this->callstackLevel, $level);
 
-        if (isset($this->fields[$key])) {
-            return $this->fields[$key];
-        } else {
-            if (isset($this->map[$key])) {
-                $dependStart = count($this->callstack);
-                $mapped = $this->map[$key];
-                if (is_object($mapped) && is_a($mapped, ExpressionInterface::class)) {
-                    $value = $mapped->calculate($this,$level+1);
-                } else {
-                    if (is_callable($mapped)) {
-                        $value = call_user_func_array($this->map[$key], [$this ,$level+1]);
-                    } else {
-                        throw new \Exception('invalid key map' . var_export($mapped, true));
-                    }
-                }
-                $keyDepend = array_slice($this->callstack, $dependStart);
-
-                foreach ($keyDepend as $dependKey) {
-                    if (!isset($this->depends[$dependKey])) {
-                        $this->depends[$dependKey] = [];
-                    }
-                    $this->depends[$dependKey][$key] = $key;
-
-                }
-
-                $this->set($key, $value);
-                return $value;
-
+        if (isset($this->map[$key])) {
+            $dependStart = count($this->callstack);
+            $mapped = $this->map[$key];
+            if (is_object($mapped) && is_a($mapped, ExpressionInterface::class)) {
+                $value = $mapped->calculate($this, $level + 1);
             } else {
-                print_r($this);
-                throw new \Exception('variable\''.$key.'\' missed ');
-                //return null;
+                if (is_callable($mapped)) {
+                    $value = call_user_func_array($this->map[$key], [$this, $level + 1]);
+                } else {
+                    throw new InvalidException('invalid key map' . var_export($mapped, true));
+                }
             }
-        }
-    }
+            $keyDepend = array_slice($this->callstack, $dependStart);
 
-    public function gets($keys , $level , $replaceDot=false){
-        if(!is_array($keys)){
-            $keys = explode(',',$keys);
-        }
-        $data = [];
-        foreach($keys as $key){
-            if($replaceDot){
-                $k = str_replace('.','_',$key);
-            }else{
-                $k = $key;
+            foreach ($keyDepend as $dependKey) {
+                if (!isset($this->depends[$dependKey])) {
+                    $this->depends[$dependKey] = [];
+                }
+                $this->depends[$dependKey][$key] = $key;
+
             }
-            $data[$k] = $this->get($key , $level);
+
+            $this->set($key, $value);
+            return $value;
+
+        } else {
+            //print_r($this);
+            throw new VariableMissingException('variable\'' . $key . '\' missed ');
+            //return null;
         }
-        return $data;
     }
 
-    public function addInterceptor($interceptor){
-        if(is_a($interceptor) == InterceptorInterface::class){
-            $this->interceptors[] = $interceptor;
-        }
-    }
-
-    public function printCallstack($return=false){
+    public function printCallstack($return = false)
+    {
         $text = '';
         foreach ($this->callstack as $index => $item) {
-            $text .= "|-".str_repeat('--',$this->callstackLevel[$index]) . $item ."\n";
+            $text .= "|-" . str_repeat('--', $this->callstackLevel[$index]) . $item . "\n";
         }
-        $text.="\n";
+        $text .= "\n";
 
-        if($return == true){
+        if ($return == true) {
             return $text;
-        }else{
+        } else {
             echo $text;
         }
+    }
+
+    /**
+     * 设置公式结果以及常量值
+     * 该函数不进行依赖检查
+     * @param \mix $key
+     * @param string $value
+     * @param string $label
+     * @return $this
+     */
+    public function set($key, $value = '', $meta = null)
+    {
+        if (is_array($key)) {
+            foreach ($key as $k => $v) {
+                $this->fields[$k] = $v;
+            }
+        } else {
+            $this->fields[$key] = $value;
+        }
+
+        $this->setMeta($key, $meta);
+        return $this;
+    }
+
+    private function setMeta($key, $meta)
+    {
+        if ($meta) {
+            if (is_string($meta)) {
+                $meta = ['label' => $meta];
+            }
+            $this->metas[$key] = $meta;
+        }
+    }
+
+    /**
+     * @param $interceptor
+     * @param string $alias
+     * @return $this
+     */
+    public function addInterceptor($interceptor, $alias = '')
+    {
+        if ($alias != '') {
+            if (isset($this->interceptors[$alias])) {
+                trigger_error(E_USER_NOTICE, 'interceptor added  skiped.');
+                return $this;
+            }
+        }
+        if (is_a($interceptor) == InterceptorInterface::class) {
+            $this->interceptors[] = $interceptor;
+        }
+        return $this;
     }
 
     public function getCallstack()
@@ -139,19 +220,19 @@ class Context
         return $this->callstack;
     }
 
-    public function getDepends($key=null)
+    public function getDepends($key = null)
     {
-        if($key){
-            return isset($this->depends[$key]) ? $this->depends[$key]:[];
-        }else
+        if ($key) {
+            return isset($this->depends[$key]) ? $this->depends[$key] : [];
+        } else
             return $this->depends;
     }
 
-    public function getMap($key=null)
+    public function getMap($key = null)
     {
-        if($key){
-            return isset($this->map[$key]) ? $this->map[$key]:[];
-        }else
+        if ($key) {
+            return isset($this->map[$key]) ? $this->map[$key] : [];
+        } else
             return $this->map;
     }
 
@@ -159,12 +240,13 @@ class Context
      * 删除域内的变量
      * @param $domain
      */
-    public function remove($domain){
+    public function remove($domain)
+    {
         $domainLength = count($domain);
         foreach ($this->fields as $k => $v) {
-                if (substr($k, 0, $domainLength) == $domain) {
-                    unset($this->fields[$k]);
-                }
+            if (substr($k, 0, $domainLength) == $domain) {
+                unset($this->fields[$k]);
+            }
         }
         return $this;
     }
@@ -173,13 +255,15 @@ class Context
      * 删除域内注册的公式
      * @param $domain
      */
-    public function unreg($domain){
+    public function unreg($domain)
+    {
         $domainLength = count($domain);
         foreach ($this->map as $k => $v) {
-                if (substr($k, 0, $domainLength) == $domain) {
-                    unset($this->map[$k]);
-                }
+            if (substr($k, 0, $domainLength) == $domain) {
+                unset($this->map[$k]);
+            }
         }
+        return $this;
     }
 
     /**
@@ -203,7 +287,7 @@ class Context
         }
         $this->callstack = [];
         $this->depends = [];
-        return $cal;
+        return $this;
     }
 
     /**
@@ -211,11 +295,20 @@ class Context
      * @param $key
      * @param $value
      */
-    public function rset($key, $value)
+    public function rset($key, $value = null)
     {
-        $this->rsetCount = 0 ;
-        $this->clearDepends($key);
-        $this->set($key, $value);
+        if (is_array($key)) {
+            $this->rsetCount = 0;
+            foreach ($key as $k => $v) {
+                $this->clearDepends($k);
+            }
+            $this->set($key);
+        }else{
+            $this->rsetCount = 0;
+            $this->clearDepends($key);
+            $this->set($key, $value);
+        }
+
         return $this;
     }
 
@@ -238,25 +331,38 @@ class Context
     }
 
     /**
-     * 设置公式结果以及常量值
-     * 该函数不进行依赖检查
-     * @param $key
-     * @param string $value
-     * @param string $label
-     * @return $this
+     * 读取多个属性以及部分meta
+     * @param $keys
+     * @return array
      */
-    public function set($key, $value = '', $meta=null)
+    public function fetchs($keys)
     {
-        if (is_array($key)) {
-            foreach ($key as $k => $v) {
-                $this->fields[$k] = $v;
-            }
-        } else {
-            $this->fields[$key] = $value;
+        if (!is_array($keys)) {
+            $keys = [$keys];
         }
-        $this->fields[$key] = $value;
-        $this->setMeta($key,$meta);
-        return $this;
+        $data = [];
+        foreach ($keys as $key) {
+            $data[$key] = [
+                'label' => $this->label($key),
+                'value' => $this->fetch($key),
+                'tips' => $this->tips($key)
+            ];
+        }
+        return $data;
+    }
+
+    /**
+     * 获取注册公式的标题
+     * @param $key
+     * @return mixed
+     */
+    public function label($key)
+    {
+        if (isset($this->metas[$key])) {
+            return isset($this->metas[$key]['label']) ? $this->metas[$key]['label'] : $key;
+        } else {
+            return $key;
+        }
     }
 
     /**
@@ -267,28 +373,21 @@ class Context
     public function fetch($key)
     {
         $this->callstack = [];
-        return $this->get($key,0);
+        return $this->get($key, 0);
     }
 
     /**
-     * 读取多个属性以及部分meta
-     * @param $keys
-     * @return array
+     * 获取注册公式的说明
+     * @param $key
+     * @return mixed
      */
-    public function fetchs($keys)
+    public function tips($key)
     {
-        if(!is_array($keys)){
-            $keys = [$keys];
+        if (isset($this->metas[$key])) {
+            return isset($this->metas[$key]['tips']) ? $this->metas[$key]['tips'] : '';
+        } else {
+            return '';
         }
-        $data = [];
-        foreach($keys as  $key){
-            $data[$key] = [
-                'label'=>$this->label($key),
-                'value'=>$this->fetch($key),
-                'tips'=>$this->tips($key)
-            ];
-        }
-        return $data;
     }
 
     /**
@@ -303,45 +402,6 @@ class Context
     }
 
     /**
-     * 注册一个公式
-     * @param $key
-     * @param $expression
-     * @param string $label
-     * @return $this
-     * @throws Exception
-     */
-    public function reg($key, $expression, $meta=null)
-    {
-        if (isset($this->map[$key])) {
-            throw new \Exception('注册的key已经存在');
-        }
-
-        if (is_string($expression)) {
-            $expression = new Expression($expression);
-            $this->map[$key] = $expression;
-        }else if(is_object($expression) && is_a($expression , ExpressionInterface::class)){
-            $this->map[$key] = $expression;
-        }else if(is_callable($expression)) {
-            $this->map[$key] = $expression;
-        }else{
-            throw new \Exception('Invalid expression for \''.$key.'\'');
-        }
-
-        $this->setMeta($key,$meta);
-        return $this;
-    }
-
-    private function setMeta($key,$meta)
-    {
-        if ($meta) {
-            if(is_string($meta)){
-                $meta =['label'=>$meta];
-            }
-            $this->metas[$key] = $meta;
-        }
-    }
-
-    /**
      * 注册一个class , key和 meta都从类中读取
      * @param $class
      * @return $this
@@ -349,36 +409,37 @@ class Context
     public function regClass($class)
     {
         $class = new $class;
-        $this->reg($class->getKey(), $class , $class->getMeta());
+        $this->reg($class->getKey(), $class, $class->getMeta());
         return $this;
     }
 
     /**
-     * 获取注册公式的标题
+     * 注册一个公式
      * @param $key
-     * @return mixed
+     * @param string|callable|ExpressionInterface $expression
+     * @param string $label
+     * @return $this
+     * @throws Exception
      */
-    public function label($key)
+    public function reg($key, $expression, $meta = null)
     {
-        if(isset($this->metas[$key])){
-            return isset($this->metas[$key]['label'])?  $this->metas[$key]['label'] : $key;
-        }else{
-            return $key;
+        if (isset($this->map[$key])) {
+            throw new KeyOccupiedException('The requested key [' . $key . '] has been registered.');
         }
-    }
 
-    /**
-     * 获取注册公式的说明
-     * @param $key
-     * @return mixed
-     */
-    public function tips($key)
-    {
-        if(isset($this->metas[$key])){
-            return isset($this->metas[$key]['tips'])?  $this->metas[$key]['tips'] : '';
-        }else{
-            return '';
+        if (is_string($expression)) {
+            $expression = new Expression($expression);
+            $this->map[$key] = $expression;
+        } else if (is_object($expression) && is_a($expression, ExpressionInterface::class)) {
+            $this->map[$key] = $expression;
+        } else if (is_callable($expression)) {
+            $this->map[$key] = $expression;
+        } else {
+            throw new InvalidException('Invalid expression for \'' . $key . '\'');
         }
+
+        $this->setMeta($key, $meta);
+        return $this;
     }
 
 }
---
+++ test/MainTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Test;
+
+use CCM\Context;
+use CCM\CyclicDependenceException;
+use CCM\VariableMissingException;
+use PHPUnit\Framework\TestCase;
+
+class MainTest extends TestCase
+{
+    public $ctx = null;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $ctx = new Context();
+        $ctx->set(['a' => 1, 'b' => 2, 'c' => 3])
+            ->reg('d', '$a + $b')
+            ->reg('e', '$a * $b')
+            ->reg('aa', '$a +5');
+        $this->ctx = $ctx;
+    }
+
+    public function testSet()
+    {
+        $this->assertEquals(1, $this->ctx->get('a', 0));
+        $this->assertEquals(2, $this->ctx->get('b', 0));
+        $this->assertEquals(2, $this->ctx->get('b', 0));
+        $this->ctx->set('b', 4);
+        $this->assertEquals(4, $this->ctx->get('b', 0));
+    }
+
+    public function testFetch()
+    {
+
+        $this->assertEquals(3, $this->ctx->fetch('c'));
+        $this->assertEquals(3, $this->ctx->fetch('d'));
+        $this->ctx->fetch('d');
+        $this->assertEquals([], $this->ctx->getCallstack()); //use cache
+
+    }
+
+    public function testFetch2()
+    {
+        $this->ctx->reg('m', '$a + $b + $d');
+        $this->assertEquals(6, $this->ctx->fetch('m'));
+        $this->assertEquals(['m', 'd'], $this->ctx->getCallstack());
+        $this->ctx->fetch('m');
+        $this->assertEquals([], $this->ctx->getCallstack()); //use cache
+    }
+
+    public function testCycleDep()
+    {
+        try {
+            $this->ctx
+                ->reg('k', '$i + $a')
+                ->reg('i', '$d + 1');
+            $this->ctx->fetch('k');
+            $this->assertEquals(1, 1);
+        } catch (\Exception $e) {
+            $this->assertEquals(get_class($e), CyclicDependenceException::class);
+        }
+
+    }
+
+    public function testMiss()
+    {
+        try {
+            $this->ctx->reg('f', '$m + 1');
+            $this->ctx->fetch('b');
+            $this->assertEquals(1, 1);
+        } catch (\Exception $e) {
+            $this->assertEquals(get_class($e), VariableMissingException::class);
+        }
+
+    }
+
+    public function testRset1(){
+        $c = new Context();
+        $c->set('a',1)
+            ->set('b',2)
+            ->reg('f','$d + $e')
+            ->reg('d','$a + $b')
+            ->reg('e','$g + 1')
+            ->reg('g','$a + 1 ')
+            ->fetch('f');
+        //$c->printCallstack();
+        $this->assertEquals(6,$c->fetch('f'));
+        $this->assertEquals(3,$c->fetch('e'));
+        $c->rset('a',100);
+        $this->assertEquals(204,$c->fetch('f'));
+        $this->assertEquals(102,$c->fetch('e'));
+    }
+    public function testRset2(){
+        $c = new Context();
+        $c->set('a',1)
+            ->set('b',2)
+            ->reg('f',function($ctx,$level) {
+                return $ctx->get('d',$level+1) + $ctx->get('e',$level+1);
+            })
+            ->reg('d','$a + $b')
+            ->reg('e','$g + 1')
+            ->reg('g',function($ctx,$level) {
+                return $ctx->get('a',$level+1) + 1;
+            })
+            ->fetch('f');
+        $this->assertEquals(6,$c->fetch('f'));
+        $this->assertEquals(3,$c->fetch('e'));
+        $c->rset('a',100);
+        $this->assertEquals(204,$c->fetch('f'));
+        $this->assertEquals(102,$c->fetch('e'));
+    }
+
+    public function testRset()
+    {
+        $this->ctx->reg('f', '$d + $e');
+        $this->assertEquals(5, $this->ctx->fetch('f'));
+        $this->assertEquals(6, $this->ctx->fetch('aa'));
+        //$this->fail(print_r($this->ctx,true));
+        $this->ctx->rset('a', 10);
+        $this->assertEquals(32, $this->ctx->fetch('f'));
+        $this->assertEquals(15, $this->ctx->fetch('aa'));
+    }
+}
